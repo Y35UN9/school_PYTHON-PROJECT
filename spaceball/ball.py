ab91b90f2c880cc848f47495347ae282734dd31e
Choi ye sung <choiyesung2848@gmail.com>
2021-12-05 05:17:13 +0900

    흐에

--- spaceball/ball.py
+++ spaceball/ball.py
@@ -1,5 +1,6 @@
 import random
 import pygame
+
 class Ball():
     def __init__(self, ball_speed = 0.5, earth_img = 'image\earth.jpg',meteor_img = 'image\meteor.jpg'):
         self.earth_img = earth_img
@@ -7,9 +8,8 @@ class Ball():
         self.ball_speed = ball_speed
        
     def ranBall(self):
-        rball = random.uniform(1.3, 2)
-        return rball
+        return random.uniform(1.3, 2)
         
     
-    def upSpeed(self):
-        self.ball_speed += 0.0001
+    # def upSpeed(self):
+    #     self.ball_speed += 0.0005
--- spaceball/maintest.py
+++ spaceball/maintest.py
@@ -1,7 +1,7 @@
 import pygame
-import ball
 import random
 import time
+import score
 
 class Board:
     def __init__(self, sound = ['mp3\spaceBall_startBGM.mp3'], board_img = ['image\start_SPACE.jpg','image\start_BALL.jpg'], board_level = 0):
@@ -9,11 +9,8 @@ class Board:
         self.sound = sound
         self.board_img = board_img
 
-    def gameOver(self):
-        return self.board_level 
-
     def soundCheck(self):
-        return self.sound
+        pygame.mixer.music.load(self.sound[0])  # 0을 board_level로 변경해야함
 
 class Checkline():
     def __init__ (self,perfect, good,checkline_img='image\checkline.png'):
@@ -23,38 +20,37 @@ class Checkline():
 
     def lineScan(self):
         global earth_pos_x
-        global earth2_pos_x
-        global earth3_pos_x
-        if nextclick_ball == 1:
-            if earth_pos_x >= 970 and earth_pos_x <= 1050:
-                earth_pos_x = -1
-                return self.perfect
-            elif earth_pos_x >= 950 and earth_pos_x <= 1070:
-                earth_pos_x = -1
-                return self.good
-        elif nextclick_ball == 2:
-            if earth2_pos_x >= 970 and earth2_pos_x <= 1050:
-                earth2_pos_x = -1
-                return self.perfect
-            elif earth2_pos_x >= 950 and earth2_pos_x <= 1070:
-                earth2_pos_x = -1
-                return self.good
-        elif nextclick_ball == 3:
-            if earth2_pos_x >= 970 and earth2_pos_x <= 1050:
-                earth2_pos_x = -1
-                return self.perfect
-            elif earth2_pos_x >= 950 and earth2_pos_x <= 1070:
-                earth2_pos_x = -1
-                return self.good
+        global click_ball
+        global earth_time
+        # if earth_x >= 870 and earth_x <= 850:
+        #     earth_x = -1
+        #     return self.perfect
+        # elif earth_x >= 750 and earth_x <= 870:
+        #     earth_x = -1
+        #     return self.good
+
+class Ball():
+    def __init__(self, ball_speed = 0.25, earth_img = 'image\earth.jpg',meteor_img = 'image\meteor.jpg'):
+        self.earth_img = earth_img
+        self.meteor_img = meteor_img
+        self.ball_speed = ball_speed
+       
+    def ranBall(self):
+        global earth_x
+        for j in range(len(earth_x)):
+            screen.blit(earth,(earth_x[j], earth_y))
+
+    def up_Speed(self):
+        return self.ball_speed
 
 pygame.init()
 pygame.display.set_caption("spaceball")
 
 voard = Board()  # board 모듈에 Board 클래스를 v(virtual)oard에 저장
-vall = ball.Ball()
+vall = Ball()
 vheckline = Checkline(10, 5)
 
-screen = pygame.display.set_mode([1280,720])                    # 게임화면 가로 세로
+screen = pygame.display.set_mode([1080,720])                    # 게임화면 가로 세로
 
 background_img = pygame.image.load('image/back_ground.jpg')     # 그냥 검정화면(화면을 초기화 하기 위한 검은색 화면, 덮는 용도임)
 background = pygame.transform.scale(background_img, (1280, 720))#
@@ -64,45 +60,39 @@ ball = pygame.image.load(voard.board_img[1])                    # 시작화면 B
 
 earth = pygame.image.load(vall.earth_img)                       # class BAll에 있는 지구 이미지
 earth = pygame.transform.scale(earth, (100, 100))               # 사이즈 조절 (지구)
-earth2 = pygame.image.load(vall.earth_img)
-earth2 = pygame.transform.scale(earth, (100, 100))
-earth3 = pygame.image.load(vall.earth_img)
-earth3 = pygame.transform.scale(earth, (100, 100))
+earth_x = []
+earth_y = 300  
+i = 0
+plus_limit = 0
 
 meteor = pygame.image.load(vall.meteor_img)                     # class BALL에 있는 메테오 이미지
 meteor = pygame.transform.scale(meteor, (140, 140))
 
 checkline = pygame.image.load(vheckline.checkline_img)
-checkline = pygame.transform.scale(checkline,(50,1200))
+checkline = pygame.transform.scale(checkline,(50,1000))
 
-earth_pos_x = -1                                                     
-earth_pos_y = 300                                              
-earth2_pos_x = -1
-earth2_pos_y = 300
-earth3_pos_x = -1
-earth3_pos_y = 300
-
-nextclick_ball = 0
+click_ball = 0
+ran = 0
 
 game_time = pygame.time.get_ticks()
 
 clock = pygame.time.Clock()                                     # 프레임 때문에 
 
 pygame.mixer.init()
-pygame.mixer.music.load(voard.sound[0])
+voard.soundCheck()
 
 pygame.mixer.music.play()
 clock.tick(100)
 while voard.board_level == 0:
     for event in pygame.event.get():
-        
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_SPACE:                          # 스페이스바를 누르면 게임시작(1)
                 voard.board_level = 1
+                start = time.time()
                 pygame.mixer.music.stop()
-                    
-    screen.blit(space,(555,180))     
-    screen.blit(ball,(555,360)) 
+
+    screen.blit(space,(455,180))     
+    screen.blit(ball,(455,360))
     pygame.display.update()
         
     if voard.board_level == 1:
@@ -112,40 +102,44 @@ while voard.board_level == 0:
 while voard.board_level == 1:
     for event in pygame.event.get():
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_END:
+            if event.key == pygame.K_ESCAPE:
                 voard.board_level = 4
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                nextclick_ball = 1
                 vheckline.lineScan()
-
+    
     running_time = (pygame.time.get_ticks() - game_time) / 1000 +1
-    screen.blit(checkline,(1020,0))
-    earth_pos_x += vall.ball_speed                                               
-    screen.blit(earth,(earth_pos_x,earth_pos_y))
+    screen.blit(checkline,(820,0))
+    ran = random.uniform(1.3, 2)
+    if time.time() - start > ran:
+        start = time.time()
+        earth_x.append(0)
+        i += 1
+        plus_limit = 1
+    if plus_limit == 1:
+        for j in range(len(earth_x)):
+            earth_x[j] += vall.up_Speed()
+        plus_limit = 0
+
+    vall.ranBall()
     pygame.display.update()
     screen.fill((0,0,0))
-    if int(running_time) % 5 == 0: # 30초 단위로 속도 증가
-        vall.upSpeed()
-    
-    if  earth_pos_x > 1200 :
-        voard.board_level = 2
-    
-    if earth_pos_x > earth2_pos_x:
-        nextclick_ball = 1
-    elif earth2_pos_x > earth3_pos_x:
-        nextclick_ball = 2
-    elif earth3_pos_x > earth_pos_x:
-        nextclick_ball = 3
 
+    if int(running_time) % 5 == 0: # 30초 단위로 속도 증가
+        vall.ball_speed += 1
+    
+    for j in range(i):
+        if  earth_x[j] > 1000:
+            voard.board_level = 2
+
+    print(earth_x)
 
 
 while voard.board_level == 2:
     for event in pygame.event.get():
         if event.type == pygame.KEYUP:
-            if event.key == pygame.K_END:
+            if event.key == pygame.K_ESCAPE:
                 voard.board_level = 4
  
 pygame.display.flip()
-
 pygame.quit()
